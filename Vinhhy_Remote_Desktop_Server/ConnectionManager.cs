a76f9d388891ccabb7e9fc8662b090bc83000105
lesonhai0339 <haileds939@gmail.com>
2025-04-09 21:54:19 +0700

    server_listen

---
+++ Vinhhy_Remote_Desktop_Server/ConnectionManager.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vinhhy_Remote_Desktop_Server
+{
+    public class ConnectionManager
+    {
+        private Socket _sck;
+        private Queue<Data> _queue;
+        private RemotesManager _remotesManager;
+        public ConnectionManager()
+        {
+            Remotes = new RemotesManager();
+            Queue = new Queue<Data>();  
+            Sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+        }
+        #region Attributes
+        public Socket Sck
+        {
+            get => _sck;
+            set
+            {
+                _sck = value;
+            }
+        }
+        public Queue<Data> Queue
+        {
+            get => _queue;
+            set
+            {
+                _queue = value;
+            }
+        }
+        public RemotesManager Remotes
+        {
+            get => _remotesManager;
+            set
+            {
+                _remotesManager = value;
+            }
+        }
+        #endregion
+        #region Functions
+        private async void ProcessQueueHandler()
+        {
+            while (true)
+            {
+                Thread.Sleep(10);
+
+                var temp = Queue.Peek();
+                if (!Remotes.IsExisted(temp.Id))
+                {
+                    Console.WriteLine($"Queue count {Queue.Count}");
+                    continue;
+                }
+                Socket sck = (temp.Type == RemoteType.OWNER)
+                            ?Remotes.Get(temp.Id).Owner 
+                            :Remotes.Get(temp.Id).Partner;
+                if(sck == null)
+                {
+                    Console.WriteLine("Remote Socket is Null");
+                    continue;
+                }
+                var queue = Queue.Dequeue();
+                int num = await SendQueueData(sck,queue);
+            }
+        }
+        public async Task<int> SendQueueData(Socket sck, Data data)
+        {
+            int result = 0;
+            switch (data.ResponseType)
+            {
+                case SocketResponseType.SCREEN:
+                    result = await sck.SendAsync(new ArraySegment<byte>(data.ByteData), SocketFlags.None);
+                    break;
+                default:
+                    break;
+
+            }
+            return result;
+        }
+        public void DataReceived(byte[] data, int length)
+        {
+            Console.WriteLine("Ok");
+        }
+        public async Task Listen(string hostName, string port)
+        {
+            if(IPAddress.TryParse(hostName, out var Ip))
+            {
+                IPEndPoint endPoint = new IPEndPoint(Ip, int.Parse(port));
+                Sck.Bind(endPoint);
+                Sck.Listen(10);
+                try
+                {
+                    while (true)
+                    {
+                        Socket clientSocket = await Task.Factory.FromAsync(
+                        Sck.BeginAccept,
+                        Sck.EndAccept,
+                        null);
+
+                        SocketClient client= new SocketClient(clientSocket, DataReceived);
+                        _ = client.StartReceiving();
+                        
+                    }
+
+
+                }
+                catch(SocketException ex)
+                {
+
+                }
+                catch(Exception ex)
+                {
+
+                }
+                finally
+                {
+                    Sck.Close();
+                }
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ Vinhhy_Remote_Desktop_Server/Entities.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vinhhy_Remote_Desktop_Server
+{
+    internal class Entities
+    {   
+    }
+    public class Data
+    {
+        public Data()
+        {
+
+        }
+        public Data(string id, RemoteType type, byte[] byteData, SocketResponseType responseType)
+        {
+            Id = id;
+            Type = type;
+            ByteData = byteData;
+            ResponseType = responseType;
+        }
+        public string Id { get; set; }
+
+        public SocketResponseType ResponseType { get; set; }
+        public RemoteType Type { get; set; }
+        public byte[] ByteData { get; set; }
+    }
+   
+    public class RemotesManager
+    {
+        private Dictionary<string, Remote> _remoteConnections;
+        private readonly object _lockObject = new object();
+
+        public RemotesManager()
+        {
+            _remoteConnections = new Dictionary<string, Remote>();
+        }
+        public void Add(string id, Socket owner, Socket partner)
+        {
+            lock (_lockObject)
+            {
+                if (_remoteConnections.TryGetValue(id, out var existingConnection))
+                {
+                    existingConnection.Owner = owner ?? existingConnection.Owner;
+                    existingConnection.Partner = partner ?? existingConnection.Partner;
+                }
+                else
+                {
+                    Remote newCon = new Remote(
+                        id: id,
+                        owner: owner,
+                        partner: partner
+                    );
+                    _remoteConnections[id] = newCon;
+                }
+            }
+        }
+        public void Remove(string id)
+        {
+            lock (_lockObject)
+            {
+                if (_remoteConnections.TryGetValue(id, out var connection))
+                {
+                    connection.Dispose();
+                    _remoteConnections.Remove(id);
+                }
+            }
+        }
+        public bool IsExisted(string id)
+        {
+            lock (_lockObject)
+            {
+                if (_remoteConnections.TryGetValue(id, out var connection))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        public Remote Get(string id)
+        {
+            lock (_lockObject)
+            {
+                if (_remoteConnections.TryGetValue(id, out var connection))
+                {
+                    return connection;
+                }
+                return null;
+            }
+        }
+        public bool UpdatePing(string id, RemoteType isOwner)
+        {
+            lock (_lockObject)
+            {
+                if (_remoteConnections.TryGetValue(id, out var connection))
+                {
+                    if (isOwner == RemoteType.OWNER)
+                    {
+                        connection.LastOwnerPing = DateTime.Now;
+                    }
+                    else
+                    {
+                        connection.LastPartnerPing = DateTime.Now;
+                    }
+                    return true;
+                }
+                return false;
+            }
+        }
+        public void Dispose()
+        {
+            lock (_lockObject)
+            {
+                foreach (var connection in _remoteConnections.Values)
+                {
+                    connection.Dispose();
+                }
+                _remoteConnections.Clear();
+            }
+        }
+    }
+    public class Remote : IDisposable
+    {
+        public Remote()
+        {
+
+        }
+        public Remote(string id, Socket owner, Socket partner)
+        {
+            ConnectionId = id;
+            Owner = owner;
+            Partner = partner;
+            LastOwnerPing = null;
+            LastPartnerPing = null;
+        }
+        public string ConnectionId { get; set; }
+        public Socket Partner { get; set; }
+        public Socket Owner { get; set; }
+        public DateTime? LastPartnerPing { get; set; }
+        public DateTime? LastOwnerPing { get; set; }
+
+        public void Dispose()
+        {
+            Owner?.Close();
+            Partner?.Close();
+            Owner?.Dispose();
+            Partner?.Dispose();
+        }
+    }
+    public class SocketClient
+    {
+        public SocketClient(Socket sck, Action<byte[], int> callback = null)
+        {
+            Socket = sck;
+            DataReceivedCallback = callback;
+        }
+        public Action<byte[], int> DataReceivedCallback { get; set; }
+        public Socket Socket { get; set; }
+        public Task<int> ReceiveAsync(byte[] buffer, int offset, int size, SocketFlags socketFlags)
+        {
+            var tcs = new TaskCompletionSource<int>();
+            Socket.BeginReceive(buffer, offset, size, socketFlags, ar => {
+                try
+                {
+                    tcs.TrySetResult(Socket.EndReceive(ar));
+                }
+                catch (Exception e)
+                {
+                    tcs.TrySetException(e);
+                }
+            }, state: null);
+            return tcs.Task;
+        }
+        public void ProcessDataHandler(byte[] buffer, int byteRead)
+        {
+            string data = Encoding.ASCII.GetString(buffer, 0, byteRead);
+            Console.WriteLine(data);
+            DataReceivedCallback?.Invoke(buffer, byteRead);
+        }
+        public async Task StartReceiving()
+        {
+            byte[] buffer = new byte[4096];
+
+            try
+            {
+                while (true)
+                {
+                    int byteRead = await ReceiveAsync(buffer, 0, buffer.Length, SocketFlags.None);
+                    if (byteRead == 0)
+                    {
+                        Console.WriteLine("Client Disconnected");
+                        break;
+                    }
+                    ProcessDataHandler(buffer, byteRead);
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                Socket.Close();
+            }
+        }
+    }
+}
