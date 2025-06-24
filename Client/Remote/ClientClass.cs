78f74f5720ac73050860306bd23727c86a9b4b8e
haile <haileds938@gmail.com>
2025-06-24 11:28:32 +0700

    up

--- Client/Remote/ClientClass.cs
+++ Client/Remote/ClientClass.cs
@@ -22,7 +22,7 @@ namespace RemoteClient.Remote
         {
             Client = remoteCLient;
             _connectionInfo = info;
-            _timer = new Timer(SendScreen, null, 0, 100000);
+            _timer = new Timer(SendScreen, null, 0, 10000);
         }
         #region Properties
         public SocketRemoteClient Client
@@ -44,23 +44,69 @@ namespace RemoteClient.Remote
         private void SendScreen(object state)
         {
             Console.WriteLine("SendScreen called at " + DateTime.Now.ToString("HH:mm:ss.fff"));
-            var x = CaptureScreen.GetScreen();
-            if (x.Any())
+            using (Bitmap capture = CaptureScreen.CaptureWindowsScreen())
             {
-                int totalBytes = x.Sum(cell => cell.Bytes.Length);
-                foreach (var cell in x)
+                byte[] bytes;
+                using (MemoryStream stream = new MemoryStream())
                 {
-                    SendScreenData(totalBytes ,cell);
+                    capture.Save(stream, ImageFormat.Png);
+                    bytes = stream.ToArray();
                 }
+                int totalBytes = bytes.Length;
+                SendScreenData1(totalBytes, bytes);
+
             }
+
+            //Console.WriteLine("SendScreen called at " + DateTime.Now.ToString("HH:mm:ss.fff"));
+            //var x = CaptureScreen.GetScreen();
+            //if (x.Any())
+            //{
+            //    int totalBytes = x.Sum(cell => cell.Bytes.Length);
+            //    Console.WriteLine("DataSend: " + totalBytes);
+            //    foreach (var cell in x)
+            //    {
+            //        SendScreenData(totalBytes ,cell);
+            //    }
+            //}
+        }
+        private void SendScreenData1(int totalBytes, byte[] byteData)
+        {
+            byte[] bytes = new byte[byteData.Length + 5];
+
+            Array.Copy(BitConverter.GetBytes(totalBytes), 0, bytes, 0, 4); // Add total bytes at the start
+            bytes[4] = 4; // Type of data, 4 for screen data
+            Array.Copy(byteData, 0, bytes, 5, byteData.Length);//real data
+
+
+            int CHUNK_SIZE = 1024;
+
+            int numberOfChunk = (int)Math.Ceiling((double)bytes.Length / CHUNK_SIZE);
+
+            for (int i = 0; i < numberOfChunk; i++)
+            {
+                int offset = i * CHUNK_SIZE;
+                int packetSize = Math.Min(CHUNK_SIZE, bytes.Length - i * CHUNK_SIZE);
+                byte[] packet = new byte[packetSize];
+
+                //data
+                Array.Copy(bytes, offset, packet, 0, packetSize);
+                if (((i + 1) % 5) == 0)
+                {
+                    Thread.Sleep(1);
+                }
+                Client.SendData(Enums.DataType.P2PDATASEND, packet);
+            }
+
         }
         private void SendScreenData(int totalBytes, CaptureCell cell)
         {
             var cellBytes = cell.Bytes;
-            byte[] bytes = new byte[cellBytes.Length + 4];
+            byte[] bytes = new byte[cellBytes.Length + 5];
 
-            Array.Copy(cellBytes, 0, bytes, 4, cellBytes.Length);
             Array.Copy(BitConverter.GetBytes(totalBytes), 0, bytes, 0, 4); // Add total bytes at the start
+            bytes[4] = 4; // Type of data, 4 for screen data
+            Array.Copy(cellBytes, 0, bytes, 5, cellBytes.Length);//real data
+
 
             int CHUNK_SIZE = 1024;
 
