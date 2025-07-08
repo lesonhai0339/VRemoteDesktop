9ed24a79fe9aca637fae8e829bc5feaa3ca9ed3a
haileds938 <haileds938@gmail.com>
2025-07-08 09:42:26 +0700

    up

--- VRemoteServer/Models/Client.cs
+++ VRemoteServer/Models/Client.cs
@@ -210,7 +210,11 @@ namespace VRemoteServer.Models
                         _dataExpected = BitConverter.ToInt32(_currentHeader, 0);
                         bytesProcessed += 5;
                         _dataReceived = 0;
-                        await ProcessData(CommandType.Header, _currentHeader);
+                        CommandType cmd = (CommandType)_currentHeader[4];
+                        if (cmd == CommandType.Data || cmd == CommandType.Screen|| (cmd == CommandType.Chunks)
+                        {
+                            await ProcessData(CommandType.Header, _currentHeader);
+                        }
                     }
                     else
                     {
