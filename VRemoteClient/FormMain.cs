d82983e03c54375240e2dcd4fa3aa13f89d32c50
haileds938 <haileds938@gmail.com>
2025-07-10 11:00:36 +0700

    up

--- VRemoteClient/FormMain.cs
+++ VRemoteClient/FormMain.cs
@@ -151,7 +151,6 @@ namespace VRemoteClient
             string data = Utils.Extensions.DataStringBuilder(new string[] { Me.ToString() });
             byte[] dataBytes = Encoding.ASCII.GetBytes(data);
             RemoteClient.Send(Models.Enums.CommandType.Login, dataBytes);
-            VScreen a= new VScreen(RemoteClient);
         }
         #endregion
 
