378ad31bd02bfd96095c707b111eb67928ea28f5
haileds938 <haileds938@gmail.com>
2025-07-15 16:50:53 +0700

    up

--- VRemoteClient/FormMain.cs
+++ VRemoteClient/FormMain.cs
@@ -25,7 +25,6 @@ namespace VRemoteClient
         private ClientInfo _clientInfo;
         private RemoteClient _remoteClient;
         private ConnectionInfo _connectionInfo;
-        private ScreenHook _screenHook;
         public FormMain()
         {
             InitializeComponent();
@@ -35,7 +34,6 @@ namespace VRemoteClient
             _resetEvent = new ManualResetEvent(false);
             Me = Utils.Extensions.InitInfo();
             RemoteClient = new RemoteClient(Me);
-            _screenHook = new ScreenHook(null);
 
             this.Text = "VRemote - Vinhhy";
             this.Icon = new Icon(@"Resources\logo.ico");
