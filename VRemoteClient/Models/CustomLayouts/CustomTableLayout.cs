47b12ae33660534164cd9a3077d0e5118b9e0437
haileds938 <haileds938@gmail.com>
2025-08-09 14:35:24 +0700

    up

---
+++ VRemoteClient/Models/CustomLayouts/CustomTableLayout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VRemoteClient.Models.CustomLayouts
+{
+    public class CustomTableLayout: CustomUserControl
+    {
+        public CustomTableLayout()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+          
+        }
+    }
+}
