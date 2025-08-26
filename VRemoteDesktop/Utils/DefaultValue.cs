2a011986ae372c19ae6dff3d6f90200bfef24ba3
haileds938 <haileds938@gmail.com>
2025-08-26 13:44:32 +0700

    change buffer size

--- VRemoteDesktop/Utils/DefaultValue.cs
+++ VRemoteDesktop/Utils/DefaultValue.cs
@@ -7,7 +7,7 @@ namespace VRemoteDesktop.Utils
 {
     public static class DefaultValue
     {
-        public static int DEFAULT_BUFFER_SIZE = 1024 * 8;
+        public static int DEFAULT_BUFFER_SIZE = 1024;
         public static int DEFAULT_CHUNK_SIZE = 1024 * 32;
         public static int DEFAULT_FPS = 20;
     }
