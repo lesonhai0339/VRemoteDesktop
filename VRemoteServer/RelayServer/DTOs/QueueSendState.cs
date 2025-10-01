ee86bca3a63a784d321ec1b49a596321ab7c13a4
haileds938 <haileds938@gmail.com>
2025-10-01 13:34:33 +0700

    Add send queue for server

---
+++ VRemoteServer/RelayServer/DTOs/QueueSendState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRemoteServer.RelayServer.DTOs
+{
+    public class QueueSendState
+    {
+        public Queue<byte[]> Queue = new Queue<byte[]>();
+        public bool IsSending = false;
+        public object LockSend = new object();
+    }
+}
